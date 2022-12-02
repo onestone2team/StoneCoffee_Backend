4d7c0b67a385439ad9718522a9d1deb5ae69c8ac
명현 <z9x80123@gmail.com>
2022-12-02 23:40:30 +0900

    add: payment model

--- order/models.py
+++ order/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from user.models import UserModel
 from product.models import Product
-from survey.models import Survey
 
 
 # Create your models here.
@@ -9,8 +8,26 @@ from survey.models import Survey
 class Order(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    payment_num = models.ForeignKey(Payment, verbose_name=("결제번호"), on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=50)
     order_price = models.IntegerField()
     count = models.IntegerField()
     created_at = models.DateField(auto_now_add=True)
-    status = models.BooleanField()
+    updated_at = models.DateField(auto_now=True)
+    status = models.IntegerField(verbose_name="주문 상태")
+    user_name = models.CharField(max_length=100)
+    user_address = models.TextField()
+    user_phone = models.CharField(max_length=15)
+    weight = models.IntegerField(verbose_name="상품 중량")
+
+
+class Payment(models.Manager):
+    product_names = models.CharField(max_length=50)
+    price = models.IntegerField()
+    count = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    status = models.IntegerField()
+    username = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
