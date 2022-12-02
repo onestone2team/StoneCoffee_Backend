12da9a1814167041fdb3305d686cbed5c1e329b0
Beam-ki <skunn123@gmail.com>
2022-12-02 10:18:36 +0900

    product column boolean >> MTM

--- product/urls.py
+++ product/urls.py
@@ -4,14 +4,14 @@ from django.conf import settings
 
 
 urlpatterns = [
-# 홈페이지 간단한 소개
-    # path('',),
-    # 카테고리 선택 (type= 0(coffeebean))
+    # 홈페이지 간단한 소개
+    path('',),
+    # 카테고리 선택 (type= 0(coffeebean),1=(goods),2=(coffeemachine))
     path('<int:type_id>/', views.ProductView.as_view()),
     # 작성은 admin만 가능
-    # path('<int:type_id>/',views.ProductView.as_view()),
+    path('create/',views.ProductView.as_view()),
     # # 게시글(제품) 조회
-    # path('<int:type_id>/',views.ProductView.as_view()),
+    path('view/<int:product_id>/',views.ProductView.as_view()),
     
 ]
 
