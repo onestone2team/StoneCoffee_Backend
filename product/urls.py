9701168a504e76d9a6f2a27c6dd0c8051b691259
Beam-ki <skunn123@gmail.com>
2022-12-02 10:38:58 +0900

    add permissins.py serializers.py

--- product/urls.py
+++ product/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     # 홈페이지 간단한 소개
     path('',),
     # 카테고리 선택 (type= 0(coffeebean),1=(goods),2=(coffeemachine))
-    path('<int:type_id>/', views.ProductView.as_view()),
+    path('<int:type_id>/', views.MainTypeView.as_view()),
     # 작성은 admin만 가능
-    path('create/',views.ProductView.as_view()),
+    path('create/',views.ProductCreateView.as_view()),
     # # 게시글(제품) 조회
-    path('view/<int:product_id>/',views.ProductView.as_view()),
+    path('<int:product_id>/view/',views.ProductView.as_view()),
+    # 게시글 좋아요
+    path('<int:product_id>/bookmark/',views.ProductLikeView.as_view())
     
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,34 +1,19 @@
 from rest_framework.views import APIView
-
+from product.models import Products, Comments, Categories
+from product.serializers import asd
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
+from product.permissions import asd
 # Create your views here.
 
+class MainTypeView(APIView):
+    pass
+class ProductCreateView(APIView):
+    pass
 class ProductView(APIView):
     pass
-    
-
-# class AddCommentView(APIView):
-#     pass
-
-
-# class PutCommentView(APIView):
-#     pass
-
-
-# class DeleteCommentView(APIView):
-#     pass
-
-
-# class CommentLikeView(APIView):
-#     pass
-
-
-# class AddCommentView(APIView):
-#     pass
-
-
-# class PutCommentView(APIView):
-#     pass
-
-
-# class DeleteCommentView(APIView):
-#     pass
+class ProductLikeView(APIView):
+    pass
