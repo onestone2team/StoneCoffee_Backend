06f10416da06edf67acb818ec4dcc7941b603947
hyojine <touroz@naver.com>
2022-12-04 15:33:52 +0900

    update: change in comment/models.py

--- comment/models.py
+++ comment/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 from product.models import Product
 from user.models import UserModel
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Comment(models.Model):
-
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='comment')
+    image = models.ImageField(blank=True, upload_to='comment')
     comment = models.TextField()
-    point = models.IntegerField()
+    point = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     like = models.ManyToManyField(Product, related_name="likes")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
 class Nested_Comment(models.Model):
-
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
--- comment/views.py
+++ comment/views.py
@@ -10,22 +10,30 @@ from rest_framework.generics import get_object_or_404
 #댓글 추가
 class CommentCreateView(APIView):
     def post(self, request, product_id):
-        serializer = CommentCreateSerializer(data=request.data)
+        serializer = CommentCreateSerializer(data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save()
+            # serializer.save(user=request.user) #이게 원래꺼 에러나서 빼뚬
             return Response({"data":serializer.data,"message":"댓글 등록 완료"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"errors":serializer.errors,"message":"댓글 등록 실패"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-#댓글 조회,수정 및 삭제
+#댓글 및 대댓글 조회,수정 및 삭제
 class CommentDetailView(APIView):
     def get(self, request, product_id, comment_id):
        comment = get_object_or_404(Comment, id=comment_id)
        serializer = CommentSerializer(comment)
        return Response(serializer.data, status=status.HTTP_200_OK)
-    def put(self, request):
-        pass
+    def put(self, request, product_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)# 이게 또 들어가는게 맞나 request.data에 다 들어가는게 아닌가??? 12/3)
+        serializer = CommentCreateSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "해당 글이 수정되었습니다.", "data": serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request):
         pass
 
