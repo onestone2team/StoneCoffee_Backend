4e233a08fbef90d4e4eb9c1f1e834b8780428f83
hyojine <touroz@naver.com>
2022-12-04 15:52:34 +0900

    refact: removed several codes related to Guest

--- survey/serializers.py
+++ survey/serializers.py
@@ -1,8 +1,3 @@
 from rest_framework import serializers
-from .models import Guest
 
-class GuestIpSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Guest
-        fields = "__all__"
