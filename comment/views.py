15a7771d18d7d5a87e048572a76748825e495b3e
hyojine <touroz@naver.com>
2022-12-05 12:11:16 +0900

    refact: 코드 및 에러/메세지 정리

--- comment/views.py
+++ comment/views.py
@@ -14,9 +14,9 @@ class CommentCreateView(APIView):
         serializer = CommentCreateSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save(product_id=product_id, user=request.user)
-            return Response({"data":serializer.data,"message":"댓글 등록 완료"}, status=status.HTTP_201_CREATED)
+            return Response({"data":serializer.data,"message":"댓글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
         else:
-            return Response({"errors":serializer.errors,"message":"댓글 등록 실패"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors":serializer.errors,"message":"댓글이 정상적으로 등록되지 않았습니다. 다시 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
 
 
 #댓글 및 대댓글 조회, 댓글 수정 및 삭제
@@ -30,7 +30,7 @@ class CommentDetailView(APIView):
         serializer = CommentCreateSerializer(comment, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "해당 댓글이 수정되었습니다.", "data": serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"data": serializer.data, "message": "해당 댓글이 수정되었습니다."}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     def delete(self, request, product_id, comment_id):
@@ -44,7 +44,7 @@ class CommentLikeView(APIView):
         comment_list = get_object_or_404(Comment, id=comment_id, product_id= product_id)
         if request.user in comment_list.like.all():
             comment_list.like.remove(request.user)
-            return Response({"message":"좋아요를 취소했습니다"}, status=status.HTTP_200_OK)
+            return Response({"message":"좋아요를 취소했습니다."}, status=status.HTTP_200_OK)
         else:
             comment_list.like.add(request.user)
             return Response({"message":"이 댓글을 좋아합니다"}, status=status.HTTP_201_CREATED)
@@ -55,9 +55,9 @@ class NestedCommentCreatetView(APIView):
         serializer = NestedCommentCreateSerializer(data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save(product_id=product_id, comment_id=comment_id, user=request.user)
-            return Response({"data":serializer.data,"message":"대댓글 등록 완료"}, status=status.HTTP_201_CREATED)
+            return Response({"data":serializer.data,"message":"대댓글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
         else:
-            return Response({"errors":serializer.errors,"message":"대댓글 등록 실패"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors":serializer.errors,"message":"대댓글이 정상적으로 등록되지 않았습니다. 다시 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
 
 #대댓글 수정 및 삭제
 class NestedCommentDetailView(APIView):
@@ -66,9 +66,9 @@ class NestedCommentDetailView(APIView):
         serializer = NestedCommentCreateSerializer(nested_comment, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response({"message": "해당 대댓글이 수정되었습니다.", "data": serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"data": serializer.data, "message": "해당 대댓글이 수정되었습니다."}, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors":serializer.errors, "message": "대댓글이 정상적으로 수정되지 않았습니다. 다시 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
     def delete(self, request, product_id, comment_id, nestedcomment_id):
         nested_comment = Nested_Comment.objects.filter(Q(id=nestedcomment_id)&Q(product_id=product_id)&Q(comment_id=comment_id)&Q(user_id=request.user.id))
         nested_comment.delete()
