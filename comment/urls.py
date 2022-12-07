d342a241a37eb69eb7bda3faa18fba69cb0a72ab
JoonPyoJang <jjpyo1009@gmail.com>
2022-12-07 23:22:09 +0900

    update: comment url 수정

--- comment/urls.py
+++ comment/urls.py
@@ -6,12 +6,9 @@ urlpatterns = [
     # 댓글
     path('<int:product_id>/comment/',views.CommentCreateView.as_view(), name='create_comment'),
     path('<int:product_id>/comment/<int:comment_id>/',views.CommentDetailView.as_view(), name='read_comment'),
-    path('<int:product_id>/comment/<int:comment_id>/',views.CommentDetailView.as_view(), name='put_comment'),
-    path('<int:product_id>/comment/<int:comment_id>/delete/',views.CommentDetailView.as_view(), name='delete_comment'),
     path('<int:product_id>/comment/<int:comment_id>/like/',views.CommentLikeView.as_view(), name='like_comment'),
     # 대댓글
     path('nested/<int:product_id>/comment/<int:comment_id>/',views.NestedCommentCreatetView.as_view(), name='create_nestedcomment'),
     path('nested/<int:product_id>/comment/<int:comment_id>/<int:nestedcomment_id>/',views.NestedCommentDetailView.as_view(), name='put_nestedcomment'),
-    path('nested/<int:product_id>/comment/<int:comment_id>/<int:nestedcomment_id>/delete/',views.NestedCommentDetailView.as_view(), name='delete_nestedcomment'),
 
 ]
