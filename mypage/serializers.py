adf2c086a9de527f71614c2d047acf3d1f9a630c
seungjuyu <2seungju0@gmail.com>
2022-12-07 14:47:37 +0900

    url수정, post 수정

---
+++ mypage/serializers.py
@@ -0,0 +1,22 @@
+from mypage.models import Inquiry
+from rest_framework import serializers
+
+
+class InquiryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inquiry
+        fields = ("status","title")
+
+
+class AddinquiryListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inquiry
+        fields = ("title", "content")
+
+
+class AddadminInquirySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inquiry
+        fields = ("answer", "status")
--- mypage/urls.py
+++ mypage/urls.py
@@ -4,6 +4,8 @@ from mypage import views
 
 urlpatterns = [
     path('inquiry/', views.InquiryList.as_view(), name='InquiryList'),
-    path('director/inquiry/', views.DirectorInquiry.as_view(), name='DirectorInquiry'),
+    path('<int:category_id>/<int:product_id>/inquiry/', views.AddinquiryList.as_view(), name='AddinquiryList'),
+    path('admin/inquiry/', views.AdminInquiry.as_view(), name='DirectorInquiry'),
+    path('admin/inquiry/<int:Inquiry_id>/', views.AddadminInquiry.as_view(), name='AddadminInquiry'),
     path('changeuserinfo/', views.ChangeUserInfo.as_view(), name='change_user_info_view'),
 ]
