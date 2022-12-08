c67d4b75588bb76b53ffec6b7bc19118a4492eb8
JoonPyoJang <jjpyo1009@gmail.com>
2022-12-09 01:17:28 +0900

    update: url로 db 데이터 가지고와서 데이터 저장 후 머신러닝

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from product.models import Product,Category, Cart
 from product.serializers import ProductSerializer, ViewProductSerializer,ProductCreateSerializer, CategorySerializer,ProductDetailSerializer, CartSaveSerializer, CartViewSerializer
 from .pagination import PageNumberPagination, get_pagination_result
-from machine.recommend import recommend_products
+from machine.recommend import recommend_products, save_dataframe
 from rest_framework import status, generics, permissions
 from rest_framework import pagination
 from rest_framework.views import APIView
@@ -115,3 +115,34 @@ class ProductCartList(APIView):
         else:
             return Response({"message":"해당 물품은 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
+import pandas as pd
+
+class ProductSave(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        id = []
+        product_name = []
+        aroma_grade = []
+        acidity_grade = []
+        sweet_grade = []
+        body_grade = []
+        for product in products:
+            if product.category.id == 1:
+                id.append(product.id)
+                product_name.append(product.product_name)
+                aroma_grade.append(product.aroma_grade)
+                acidity_grade.append(product.acidity_grade)
+                sweet_grade.append(product.sweet_grade)
+                body_grade.append(product.body_grade)
+        newdata = {}
+        newdata["num"]=id
+        newdata["name_ko"]=product_name
+        newdata["aroma_grade"]=aroma_grade
+        newdata["acidity_grade"]=acidity_grade
+        newdata["sweet_grade"]=sweet_grade
+        newdata["body_grade"]=body_grade
+        df = pd.DataFrame(newdata)
+        df.to_csv("./machine/dbdata.csv", index=False, encoding='cp949')
+        save_dataframe()
+        return Response({"message":"저장되었습니다."}, status=status.HTTP_200_OK)
+       
